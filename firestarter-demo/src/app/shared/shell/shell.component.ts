301bd6ac9c24a75b5658ed129640d77793e587e6
Kelvin Chin <chinkaiwen@yahoo.com>
2023-01-15 02:00:54 +0800

    changed di injection method

--- firestarter-demo/src/app/shared/shell/shell.component.ts
+++ firestarter-demo/src/app/shared/shell/shell.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -9,12 +9,12 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
   public isHandset$: Observable<boolean> = this.breakpointObserver
     .observe([Breakpoints.Handset])
     .pipe(
       map((result) => result.matches),
       shareReplay()
     );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
 }
