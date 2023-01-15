5cb33c5afd4624a8952281235848f143d8a41451
Kelvin Chin <chinkaiwen@yahoo.com>
2023-01-16 01:59:36 +0800

    prepared for email signin course and added custom font

--- firestarter-demo/src/app/user/user.module.ts
+++ firestarter-demo/src/app/user/user.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSgininDirective } from './google-sginin.directive';
-
+import { EmailLoginComponent } from './email-login/email-login.component';
 @NgModule({
-  declarations: [
-    LoginPageComponent,
-    GoogleSgininDirective
-  ],
-  imports: [CommonModule, UserRoutingModule, SharedModule],
+  declarations: [LoginPageComponent, GoogleSgininDirective, EmailLoginComponent],
+  imports: [CommonModule, UserRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class UserModule {}
