444e12c94fc2e08460275761d2f9d7f066e6a092
Kelvin Chin <chinkaiwen@yahoo.com>
2023-01-18 02:46:33 +0800

    Completed dialogs course

--- firestarter-demo/src/app/kanban/board-list/board-list.component.ts
+++ firestarter-demo/src/app/kanban/board-list/board-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 import { BoardService } from '../board.service';
 import { Board } from '../board.modal';
+import { BoardDialogComponent } from '../dialogs/board-dialog.component';
 
 @Component({
   selector: 'app-board-list',
@@ -12,6 +14,7 @@ import { Board } from '../board.modal';
 })
 export class BoardListComponent implements OnInit, OnDestroy {
   public boardService = inject(BoardService);
+  public dialog = inject(MatDialog);
 
   public boards!: Board[];
   private boardSubscription!: Subscription;
@@ -32,5 +35,19 @@ export class BoardListComponent implements OnInit, OnDestroy {
   drop(ev: CdkDragDrop<string[]>) {
     moveItemInArray(this.boards, ev.previousIndex, ev.currentIndex);
     this.boardService.sortBoards(this.boards);
+  }
+
+  openBoardDialog(): void {
+    const dialog = this.dialog.open(BoardDialogComponent, {
+      width: '400px',
+      data: {},
+    });
+    dialog.afterClosed().subscribe((result) => {
+      if (!result) return;
+      this.boardService.createBoard({
+        title: result,
+        priority: this.boards.length,
+      });
+    });
   }
 }
--- firestarter-demo/src/app/kanban/board/board.component.ts
+++ firestarter-demo/src/app/kanban/board/board.component.ts
@@ -1,8 +1,10 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, inject, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
-import { Board } from '../board.modal';
+import { Board, Task } from '../board.modal';
 import { BoardService } from '../board.service';
+import { TaskDialogComponent } from '../dialogs/task-dialog.component';
 
 @Component({
   selector: 'app-board',
@@ -14,9 +16,33 @@ export class BoardComponent {
   public board!: Board;
 
   public boardService = inject(BoardService);
+  private dialog = inject(MatDialog);
 
   taskDrop(ev: CdkDragDrop<string[]>) {
     moveItemInArray(this.board.tasks!, ev.previousIndex, ev.currentIndex);
     this.boardService.updateTasks(this.board.id!, this.board.tasks!);
+  }
+
+  openDialog(task?: Task, idx?: number): void {
+    const newTask: Task = { label: 'purple' };
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '500px',
+      data: task
+        ? { task: { ...task }, isNew: false, boardID: this.board.id, idx }
+        : { task: newTask, isNew: true },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!result) return;
+      if (result.isNew) {
+        this.boardService.updateTasks(this.board.id!, [
+          ...this.board.tasks!,
+          result.task,
+        ]);
+      } else {
+        const update = this.board.tasks;
+        update?.splice(result.idx, 1, result.task);
+        this.boardService.updateTasks(this.board.id!, this.board.tasks!);
+      }
+    });
   }
 }
